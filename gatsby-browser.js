3f609dd7520b78cec7023b1c79e7577c99ce6bb9
Qin <level7in@gmail.com>
2020-04-17 16:54:55 +0800

    fix: zeit build fail

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,5 +2,5 @@
 import "typeface-montserrat"
 import "typeface-merriweather"
 
-import "prism-themes/themes/prism-shades-of-purple.css"
+import "prismjs/themes/prism-tomorrow.css"
 import "prismjs/plugins/line-numbers/prism-line-numbers.css"
